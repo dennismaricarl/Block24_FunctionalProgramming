649f42d92c6a39134518b46fcb84c08014dddc33
Mari <dennismaricarl@gmail.com>
2023-09-09 09:14:33 -0700

    all 3 Tabs_progress

---
+++ index.js
@@ -0,0 +1,159 @@
+//TAB1: First-class Function  
+
+//#1
+    //create variable 
+    //assign a function to variable
+    //use function to return another function that converts a string to uppercase
+
+const convertString = () => (string) => {
+    return string.toUpperCase();
+ };
+
+const toUpperCase = convertString();
+const word = "welcome";
+const word_in_uppercase = toUpperCase(word);
+
+console.log(word_in_uppercase);
+
+
+//#2 
+ //create a variable 
+ //assign a function to that variable that takes "inputNumber"
+ //that function will return another function that multiplies inputNumber with number 
+
+//do partially apply 
+//get a function that will call the next product 
+
+const multiplyNumbers = (inputNumber) => (number) => {
+    return inputNumber * number
+};
+
+const function_that_multiplies = multiplyNumbers();
+const input_number = multiplyNumbers(3);
+const product = input_number(7);
+
+console.log(product)
+
+//#3
+
+// const ObjectReturned = () => (num1, num2) => {
+//     const result1 = num1;
+//     const result2 = num2;
+//     const object = {
+//         result1: result1, 
+//         result2: result2
+//     }
+
+//     return object;
+// };
+
+
+// const myObjectReturned = ObjectReturned();
+// const add2 = myObjectReturned() 
+// const 
+// const results = myObjectReturned(add2, add4)
+
+
+// console.log(results )
+
+//TAB2: HIGHER-ORDER FUNCTION 
+
+
+//write array
+//function that takes array 
+//and modify each element in the input 
+//return a new array with modified elements
+
+//#1
+const animals  =  ['Hippopotamus', 'King Cobra', 'Giant Panda', 'Crocodile'];
+
+const mappedArray = animals.map(function(animal){
+    return animal.toLowerCase();
+});
+
+console.log(mappedArray)
+
+//#2
+    //write array
+    //create variable, equal to funciton
+    //use .map to iterate through list 
+    //use if statement to find string that has more than 11 characs
+    //return that item
+
+const moreThan11 = animals.map(function(animal) {
+    if(animal.length > 11) {
+        return animal
+    }
+});
+
+console.log(moreThan11);
+
+
+//#3
+    //write array
+    //create function that takes a string 
+    //use map to iterate over each item in array
+    //split the string, reverse the order, and join back together
+    //use reverStrings function and pass animals ? 
+const reverseStrings = (animals) => animals.map((animal) => animal.split('').reverse().join(''));
+
+const reversedAnimals = reverseStrings(animals)
+console.log(reversedAnimals)
+
+//#4
+    //first function - 
+        //takes speed and name
+        //return an array, value:pairs? 
+
+    //second function - 
+        //takes runner1, runner2, and race distance
+        //whoever is faster and has greater distance is winner 
+            //if speed and distance are greater, return that runner
+            //if equal, return 'It's a tie'
+
+// runners = [];
+// const function1 = (speed, name) => {
+//     runners = [];
+//     name.map()
+// }
+
+
+//TAB 3: CURRYING FUNCTION
+
+//separate parts of URL 
+    //protocol = https
+    //subdomain = www 
+    //domain = fullstackacademy
+    //top-level domain = .com 
+    //endpoint = weather 
+
+//create function that takes every part as paramter 
+    //divide function into several ones. each will take one parameter
+    //return will concatenate/add every part of the URL 
+    //call by putting arguments side by side ()()()
+
+// function add(protocol){
+//     return function (subdomain) {
+//         return function (domain){
+//             return function (topLevel) {
+//                 return function (endpoint) {
+//                     return protocol + subdomain + '.' + domain + '.' + topLevel + endpoint;
+//                 }
+//             }
+//         }
+//     }
+// };
+
+const add = (protocol) => (subdomain) => (domain) => (topLevel) => (endpoint) =>
+    `${protocol}${subdomain}.${domain}.${topLevel}${endpoint}`;
+
+const concat = add('https://')('www')('fullstackacademy')('com')('/weather');
+const concat2 = ('/stocks')
+const concat2_URL = concat + concat2 
+console.log(concat)
+console.log(concat2_URL)
+
+
+
+
+
